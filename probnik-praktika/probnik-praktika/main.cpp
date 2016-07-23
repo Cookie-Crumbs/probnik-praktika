4fe7752329fffa82d3178d3e36e9c2050f050d73
Cookie-Crumbs <cookies.from.gouaches@gmail.com>
2016-07-23 20:46:48 +0300

    добавлено: -возможность установки соединения фигурок

--- probnik-praktika/probnik-praktika/main.cpp
+++ probnik-praktika/probnik-praktika/main.cpp
@@ -30,12 +30,21 @@ int main()
 
 	//контейнер details - хранит в себе все созданные детали
 	map<int, Player> details;
-	int count = 0, N = 0, D=0,X,ud=0;//количество деталей
-	
+	int count = 0, N = 0, D = 0, X, ud = 0, v = 0;//количество деталей
+	int V[5] = {};
 
-	Player KV1("kv1.png", 75, 56, 127.0, 127.0,"kv1");
-	Player KR1("kr1.png", 75, 56, 102.0, 102.0,"kr1");
-	Player Z1("z1.png", 75, 56, 25.0, 25.0,"z1");
+	b2BodyDef bdef;//переменая свойств тела
+	bdef.type = b2_dynamicBody;
+	b2Body *bodyV = World.CreateBody(&bdef);
+	b2CircleShape circle;
+	circle.m_radius = (102.0 / 2) / SCALE;// разве тут не 102/2 ? 
+	bodyV = World.CreateBody(&bdef);//CreateBody(&bdef) создаем тело, закидываем в m_world, делаем ссылку на тело
+	bodyV->CreateFixture(&circle, 1); //прикрепляем фигуру к телу
+
+	b2Vec2 Q(10.0f, 0.0f);
+	Player KV1("kv1.png", 75, 56, 127.0, 127.0, "kv1");
+	Player KR1("kr1.png", 75, 56, 102.0, 102.0, "kr1");
+	Player Z1("z1.png", 75, 56, 25.0, 25.0, "z1");
 
 	Clock clock; //создаем переменную времени
 	bool isMove = false;//переменная для щелчка мыши по спрайту
@@ -155,8 +164,8 @@ int main()
 			window.draw(fon);
 			window.draw(ico_kv1);
 			window.draw(ico_kr1);
-			window.draw(ico_z1); 
-			window.draw(ico_c); 
+			window.draw(ico_z1);
+			window.draw(ico_c);
 			if (count){
 				for (int i = 1; i <= count; i++){
 					if (details.at(i).del == false)
@@ -170,24 +179,24 @@ int main()
 		if (sozdanie == false){
 
 			if (f2){
-			
+
 				setWall(490, 463, 490, 37);// 74||37 426||463
 
-				
+
 				//тут создание тел спрайтов
 				if (count){
 
 					b2BodyDef bdef;//переменая свойств тела
 					bdef.type = b2_dynamicBody;
-					std::vector<b2Body *>body(count-ud);	
+					std::vector<b2Body *>body(count - ud);
 					b2Body *body3 = World.CreateBody(&bdef);
-					
+
 					int bb = 0;//индекс элемента ссылки тела
 					double Cx, Cy;
 					//bdef.position.Set(4, 20);
 					//
-					b2RevoluteJointDef rJoint;
-					
+					b2RevoluteJointDef jointDef;
+
 					for (int i = 1; i <= count; i++){
 
 						if (details.at(i).del == false)
@@ -197,17 +206,22 @@ int main()
 
 							//круг
 							if (details.at(i).t == "kr1"){
-							
-								
+
+
 								b2CircleShape circle;
 								details.at(i).sprite.setOrigin(51, 51);
-								circle.m_radius = (102.0/2) / SCALE;// разве тут не 102/2 ? 
-								bdef.position.Set((details.at(i).x +51) / SCALE, (details.at(i).y+51) / SCALE);
+								circle.m_radius = (102.0 / 2) / SCALE;// разве тут не 102/2 ? 
+								bdef.position.Set((details.at(i).x + 51) / SCALE, (details.at(i).y + 51) / SCALE);
 
 								body[bb] = World.CreateBody(&bdef);//CreateBody(&bdef) создаем тело, закидываем в m_world, делаем ссылку на тело
 								body[bb]->CreateFixture(&circle, 1); //прикрепляем фигуру к телу
 
 								body[bb]->SetUserData("Box");
+								v = bb;
+								bodyV = body[bb];
+								//bodyV->SetAngularVelocity(0.5f);
+
+
 								bb++;
 
 							}
@@ -216,16 +230,17 @@ int main()
 							if (details.at(i).t == "kv1"){
 
 								b2PolygonShape shape;//создаем фигуру
-								shape.SetAsBox(127.0 / SCALE, 127.0 / SCALE); //квадратик
-								bdef.position.Set(details.at(i).x / SCALE, details.at(i).y / SCALE);
+								details.at(i).sprite.setOrigin(63.5, 63.5);
+								shape.SetAsBox(63.5 / SCALE, 63.5 / SCALE); //квадратик
+								bdef.position.Set((details.at(i).x + 63.5) / SCALE, (details.at(i).y + 63.5) / SCALE);
 
 								body[bb] = World.CreateBody(&bdef);//CreateBody(&bdef) создаем тело, закидываем в m_world, делаем ссылку на тело
-								body[bb]->CreateFixture(&shape, 1); //прикрепляем фигуру к телу
+								body[bb]->CreateFixture(&shape, 10); //прикрепляем фигуру к телу
 								body[bb]->SetUserData("Box");
-								
+
 
 								bb++;
-								
+
 								//body[0] = m_world->CreateBody(&bdef);//CreateBody(&bdef) создаем тело, закидываем в m_world, делаем ссылку на тело
 								//body[0]->CreateFixture(&shape, 1); //прикрепляем фигуру к телу
 
@@ -236,48 +251,97 @@ int main()
 
 								b2CircleShape soed;
 								details.at(i).sprite.setOrigin(12.5, 12.5);
-								soed.m_radius = (25.0/2) / SCALE;
+								soed.m_radius = (25.0 / 2) / SCALE;
 
 								body[bb] = World.CreateBody(&bdef);//CreateBody(&bdef) создаем тело, закидываем в m_world, делаем ссылку на тело
 								body[bb]->CreateFixture(&soed, 1); //прикрепляем фигуру к телу
 								body[bb]->SetUserData("Box");
+								//body[bb]->SetLinearVelocity(Q);
+								//v = bb;
+								for (int j = 1; j <= count; j++){
+									if (details.at(j).t != "z1"){
 
-								//for (int j = 1; j <= count; j++){ 
-								//	if (details.at(j).t != "z1"){
-								//		if (details.at(j).t == "kr1")
-								//			if ((pow((details.at(i).x - (details.at(j).x + 51)), 2) + pow((details.at(i).y - (details.at(j).y+51)), 2)) <= (pow(51.0, 2))){
-								//				//понять в каком месте
-								//				//Cx = (details.at(i).x - details.at(j).x);
-								//				//Cy = (details.at(i).y - details.at(j).y);
-								//				//от середины фигуры до точки
+										if (details.at(j).t == "kr1")
+										{
+											if ((pow((details.at(i).x - (details.at(j).x + 51)), 2) + pow((details.at(i).y - (details.at(j).y + 51)), 2)) <= (pow(51.0, 2))){
+												//понять в каком месте
+												Cx = (details.at(i).x - ((details.at(j).x) + 51));
+												Cy = (details.at(i).y - ((details.at(j).y) + 51));
+												//от середины фигуры до точки
 
-								//				//прикрепиться к тому спрайту (фигуре,телу) шо пересекает
-								//				rJoint.bodyA = body[bb];
-								//				rJoint.bodyA = body[details.at(j).BB];
-								//				//rJoint.localAnchorA.Set(0, 0);
-								//				//rJoint.localAnchorB.Set(0, 0);
 
-								//				//World.CreateJoint(&rJoint);
-								//				/*
-								//				rJoint.bodyA = body2;
-								//				rJoint.bodyB = body3;
+												//прикрепиться к тому спрайту (фигуре,телу) шо пересекает
+												jointDef.Initialize(body[details.at(j).BB], body[bb], body[details.at(j).BB]->GetWorldCenter());
+												//jointDef.localAnchorA.Set(0,0);
+												//jointDef.localAnchorB.Set(0, 0);//51  / SCALE
+												World.CreateJoint(&jointDef);
+												//body[bb]->SetAngularVelocity(0.5f);
+											}
+										}
+										else if (details.at(j).t == "kv1"){
+											if ((details.at(i).x >= details.at(j).x) && (details.at(i).y >= details.at(j).y) && (details.at(i).x <= (details.at(j).x + 127)) && (details.at(i).y <= (details.at(j).y + 127))){//понять принадлежность квадрату
 
-								//				rJoint.localAnchorA.Set(4, 10);
-								//				rJoint.localAnchorB.Set(5, 0);
 
-								//				m_world->CreateJoint(&rJoint);//закидываем в m_world соединение
-								//				*/
-								//		}
 
-								//	//	if (details.at(j).getRect ){ ; }//if (враг.getRect().intersects(игрок.getRect())) будет равно истине (true).
-								//	}
-								//}
+												if (details.at(i).x >= (details.at(j).x + 63)){
+
+													jointDef.Initialize(body[bb], body[details.at(j).BB], body[bb]->GetWorldCenter());
+													jointDef.localAnchorA.Set(0, 0);
+
+													jointDef.localAnchorB.Set(-63.5 / SCALE, 0);//0,0 середина | (0, 63.5/SCALE) - verh/niz | (63.5 / SCALE,0) -levo/pr |
+													World.CreateJoint(&jointDef);
+												}
+												else if (details.at(i).x <= (details.at(j).x + 30)){
+
+													jointDef.Initialize(body[bb], body[details.at(j).BB], body[bb]->GetWorldCenter());
+													jointDef.localAnchorA.Set(0, 0);
+
+													jointDef.localAnchorB.Set(63.5 / SCALE, 0);//0,0 середина | (0, 63.5/SCALE) - verh/niz | (63.5 / SCALE,0) -levo/pr |
+													World.CreateJoint(&jointDef);
+												}
+												else if (details.at(i).y >= (details.at(j).y + 63)){
+
+													jointDef.Initialize(body[bb], body[details.at(j).BB], body[bb]->GetWorldCenter());
+													jointDef.localAnchorA.Set(0, 0);
+
+													jointDef.localAnchorB.Set(0, -63.5 / SCALE);//0,0 середина | (0, 63.5/SCALE) - verh/niz | (63.5 / SCALE,0) -levo/pr |
+													World.CreateJoint(&jointDef);
+
+													/*//кр+кв
+													jointDef.Initialize(body[v], body[details.at(j).BB], body[v]->GetWorldCenter());
+													jointDef.localAnchorA.Set(0, 0);
+
+													jointDef.localAnchorB.Set(0, (-63.5 + 51.0) / SCALE);//0,0 середина | (0, 63.5/SCALE) - verh/niz | (63.5 / SCALE,0) -levo/pr |
+													World.CreateJoint(&jointDef);*/
+
+												}
+												else if (details.at(i).y <= (details.at(j).y + 30)){
+
+													jointDef.Initialize(body[bb], body[details.at(j).BB], body[bb]->GetWorldCenter());
+													jointDef.localAnchorA.Set(0, 0);
+
+													jointDef.localAnchorB.Set(0, 63.5 / SCALE);//0,0 середина | (0, 63.5/SCALE) - verh/niz | (63.5 / SCALE,0) -levo/pr |
+													World.CreateJoint(&jointDef);
+												}
+
+												//прикрепиться к тому спрайту (фигуре,телу) шо пересекает
+												//details.at(j).sprite.setOrigin(63.5, 63.5);
+
+												//body[bb]->SetAngularVelocity(0.5f);
+
+												//понять к какой стенке его пришили
+												//пришиться
+											}
+										}
+									}
+
+								}
 
 								bb++;
 								//body[2] = m_world->CreateBody(&bdef);//CreateBody(&bdef) создаем тело, закидываем в m_world, делаем ссылку на тело
 								//body[2]->CreateFixture(&soed, 1); //прикрепляем фигуру к телу
 
-								
+
 								//найти середину спрайта
 
 								//проверить пересекает ли он другие спрайты (проверка по всем спрайтам)
@@ -285,11 +349,29 @@ int main()
 								//если да
 								//понять в каком месте
 								//прикрепиться к тому спрайту (фигуре,телу) шо пересекает
-								
+								/////////////////////////////////////
+
+								/*for (int ii = 0,cr=0,cv=0; ii < v; ii++){
+								if (details.at(V[ii]).t == "kv1")
+								cv = V[ii];
+								if (details.at(V[ii]).t == "kr1")
+								cr = V[ii];
+
+								if (ii == v - 1)
+								{
+								jointDef.Initialize(body[details.at(cr).BB], body[details.at(cv).BB], body[details.at(cr).BB]->GetWorldCenter());
+								jointDef.localAnchorA.Set(0, 0);
+
+								jointDef.localAnchorB.Set(0, -63.5 / SCALE);//0,0 середина | (0, 63.5/SCALE) - verh/niz | (63.5 / SCALE,0) -levo/pr |
+								World.CreateJoint(&jointDef);
+								}
+								}*/
+
 							}
 							//b_ground->SetUserData("Box")
 
 						}
+						//body[v]->SetAngularVelocity(1.5f);
 					}
 				}
 
@@ -306,24 +388,24 @@ int main()
 
 			window.clear();
 			window.draw(fon2);
-
+			bodyV->SetAngularVelocity(0.5f);
 			X = 0;
 			if (count){
 				for (b2Body* it = World.GetBodyList(); it != 0; it = it->GetNext()){//итератор пробегает по всем телам
 
 					X++;
-					
+
 					if (it->GetUserData() == "Box")//если это деталька
 					{
-					while (details.at(X).del == true)
-					{
-						X++;
-					}
-					b2Vec2 pos = it->GetPosition();
-					float angle = it->GetAngle();//ugol
-					details.at(X).sprite.setPosition(pos.x*SCALE, pos.y*SCALE);
-					details.at(X).sprite.setRotation(angle*DEG);
-					window.draw(details.at(X).sprite);
+						while (details.at(X).del == true)
+						{
+							X++;
+						}
+						b2Vec2 pos = it->GetPosition();
+						float angle = it->GetAngle();//ugol
+						details.at(X).sprite.setPosition(pos.x*SCALE, pos.y*SCALE);
+						details.at(X).sprite.setRotation(angle*DEG);
+						window.draw(details.at(X).sprite);
 					}
 				}
 			}
